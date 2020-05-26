fd97021f52db9c2afc9013ff81ceb8a586c68d63
Unknown <hemaxi.1996@gmail.com>
2020-05-26 16:50:32 +0100

    1st commit

---
+++ main.m
@@ -0,0 +1,18 @@
+
+a = imread('C:\Users\hemaxi\Desktop\b.png');  
+a = a(:,:,1);
+a = imresize(a,[128,128], 'nearest'); 
+N = 10; % filter size
+[gx,gy,gradient_img] = compute_gradient(a);
+radial_directions = iris_radial_directions(6);
+conv_map = iris_filter(gradient_img, gx, gy, 6,radial_directions);
+
+figure;
+imagesc(conv_map);
+th = 0.3;
+C = conv_map;
+D = imregionalmax(C,8);
+D = D.*(C>th*max(C(:)));
+hold on
+[i,j]=find(D);
+plot(j,i,'.r')
